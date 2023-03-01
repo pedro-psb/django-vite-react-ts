b09d8b4a182a4dd381f972ffce6492c4c11eb448
pedro-psb <pedropsb95@gmail.com>
2023-03-01 18:37:21 -0300

    add dev.sh to run both servers on tmux

---
+++ dev.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# run dev server for django and vite
+# ref: https://how-to.dev/how-to-create-tmux-session-with-a-script
+
+session="django-vite"
+run_vite="npm run dev"
+run_django="source .venv/bin/activate && python3 manage.py runserver"
+# run_vite="echo hola"
+# run_django="echo hola-again"
+
+tmux kill-server
+# tmux kill-session $session
+
+tmux new-session -d -s $session
+tmux rename-window -t $session:1 'vite'
+tmux send-keys -t $session:1 "$run_vite" Enter
+tmux split-window -h
+tmux send-keys -t $session:1 "$run_django" Enter
+
+tmux attach-session -t $session
+
